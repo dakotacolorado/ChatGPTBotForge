d86bf269c7d70f823195ff16c784305d28e3307a
Dakota Parker <dakotacolorado2@gmail.com>
2023-09-07 22:29:02 -0700

    add prof woof as heart beat alarm

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import { ChefWoof } from './dakotas-bots/ChefWoof'
 import { continuallyDeleteMessages } from './discord/deleteMessages'
-import { Website } from './website/Website'
-import { WordleBot } from './dakotas-bots/WordleBot'
+
+import { ProfessorWoof } from './dakotas-bots/ProfessorWoof'
 require('dotenv').config()
 
 new ChefWoof(process.env.OPENAI_API_KEY, process.env.DISCORD_TOKEN_CHEF_WOOF, process.env.WOOFS_KITCHEN_CHAT)
 
 continuallyDeleteMessages(process.env.DISCORD_TOKEN_DAKOTAS_BOT, process.env.SPEAK_EASY_CHAT, 24, 10)
 
-// const professor = new ProfessorWoof(process.env.OPENAI_API_KEY, process.env.DISCORD_TOKEN_DAKOTAS_BOT,  process.env.WOOFS_CLASSROOM_CHAT)
-// professor.scheduleQuestion("SQL", Math.floor(Math.random()*6 + 5).toString(), 6 * 60 * 60 * 1000) // 6 hours in milliseconds
+const professor = new ProfessorWoof(process.env.OPENAI_API_KEY, process.env.DISCORD_TOKEN_DAKOTAS_BOT, process.env.WOOFS_CLASSROOM_CHAT)
+professor.scheduleQuestion('SQL', Math.floor(Math.random() * 6 + 5).toString(), 6 * 60 * 60 * 1000) // 6 hours in milliseconds
 
 // wordy.playWordleOnSchedule( 24 * 60 * 60 * 1000 )// 24 hours in milliseconds
 // const wordy = new WordleBot(
--- typings/env.d.ts
+++ typings/env.d.ts
@@ -8,6 +8,7 @@ declare namespace NodeJS {
         TECH_SCIENCE_CODING_CHAT: string;
         WOOFS_KITCHEN_CHAT: string;
         WOOFS_CLASSROOM_CHAT: string;
+        SPEAK_EASY_CHAT: string;
         OPENAI_API_KEY: string;
         AWS_REGION: string;
         AWS_ACCESS_KEY_ID: string;
